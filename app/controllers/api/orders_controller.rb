9582617ca69d650c9fc4a1b57c59a5e5f59acc49
Joe Gallotta <joeygallotta@gmail.com>
2018-10-31 20:34:22 -0500

    hardcode new Order create action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,6 @@
 class Api::OrdersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_with_http_digest
+  # authenticate_with_http_digest
   def index
    # @orders = Order.where(user_id: current_user.id)
     if current_user
@@ -10,37 +11,44 @@ class Api::OrdersController < ApplicationController
     end
   end
 
-def create
-    # most basic create action of all time
-    # subtotal
-    # subtotal == quantity * price
-    product = Product.find_by(id: params[:product_id])
-
-    price = product.price
-
-    calculated_subtotal = params[:quantity].to_i * price
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
-    @order = Order.new(
-      product_id: params[:product_id],
-      user_id: current_user.id,
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      total: calculated_total,
-      tax: calculated_tax
-    )
-    @order.save
-    # how can i figure out why this isn't saving
-    p @order.errors.full_messages
-    render "show.json.jbuilder"
-
-    # intake params
-    # Order.new something something
-    # order.save something something
-    # render the new thing I jut saved
+  def create
+    @user = User.first
+    @user = @user.carted_products
+    render 'index.json.jbuilder'
   end
 end
+
+# def create
+#     # most basic create action of all time
+#     # subtotal
+#     # subtotal == quantity * price
+#     product = Product.find_by(id: params[:product_id])
+
+#     price = product.price
+
+#     calculated_subtotal = params[:quantity].to_i * price
+#     calculated_tax = calculated_subtotal * 0.09
+#     calculated_total = calculated_subtotal + calculated_tax
+
+#     @order = Order.new(
+#       product_id: params[:product_id],
+#       user_id: current_user.id,
+#       quantity: params[:quantity],
+#       subtotal: calculated_subtotal,
+#       total: calculated_total,
+#       tax: calculated_tax
+#     )
+#     @order.save
+#     # how can i figure out why this isn't saving
+#     p @order.errors.full_messages
+#     render "show.json.jbuilder"
+
+#     # intake params
+#     # Order.new something something
+#     # order.save something something
+#     # render the new thing I jut saved
+#   end
+
  
 
 
--- app/views/api/orders/index.json.jbuilder
+++ app/views/api/orders/index.json.jbuilder
@@ -1,9 +1,10 @@
-json.array! @orders do |order|
-  json.id order.id
-  json.product_id order.product_id
-  json.user_id order.user_id
-  json.subtotal order.subtotal
-  json.total order.total
-  json.tax order.tax
-  json.quantity order.quantity
-end
+# json.array! @orders do |order|
+#   json.id order.id
+#   json.product_id order.product_id
+#   json.user_id order.user_id
+#   json.subtotal order.subtotal
+#   json.total order.total
+#   json.tax order.tax
+#   json.quantity order.quantity
+# end
+json.message @user
