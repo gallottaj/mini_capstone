ccd8d404cbe259cfc03aee0b97e602103f3f4a01
Joe Gallotta <joeygallotta@gmail.com>
2018-10-31 20:16:31 -0500

    add CartedProduct index action (current_user)

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,37 +1,28 @@
 class Api::CartedProductsController < ApplicationController
+# before_action :authenticate_user
+
+  def index
+    @cart = CartedProduct.where(user_id: current_user.id)
+    if current_user
+      @cart = current_user.carted_products
+      render "index.json.jbuilder"
+    else
+      render json: [], status: :unauthorized
+    end
+  end
+
+ # Make a CartedProducts index action. Note that this shouldn’t show ALL the carted products - it should only display the current user’s carted products that have a status of “carted”!
+
 
   def create
     product = Product.find_by(id: params[:product_id])
     cart = CartedProduct.new(product_id: params[:product_id], user_id: current_user.id, quantity: params[:quantity])
     cart.save!
-    # render 'show.json.jbuilder'
     if cart.save
-      render json: {message: 'Carted successfully'}, status: :created
+      render json: {message: 'Carted!'}, status: :carted
     else
       render json: {errors: cart.errors.full_messages}, status: :bad_request
     end
   end
 end
 
-
-
-# product = Product.find_by(id: params[:product_id])
-
-#     price = product.price
-
-#     calculated_subtotal = params[:quantity].to_i * price
-#     calculated_tax = calculated_subtotal * 0.09
-#     calculated_total = calculated_subtotal + calculated_tax
-
-#     @order = Order.new(
-#       product_id: params[:product_id],
-#       user_id: current_user.id,
-#       quantity: params[:quantity],
-#       subtotal: calculated_subtotal,
-#       total: calculated_total,
-#       tax: calculated_tax
-#     )
-#     @order.save
-#     # how can i figure out why this isn't saving
-#     p @order.errors.full_messages
-#     render "show.json.jbuilder"
