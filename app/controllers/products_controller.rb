456e6b918fbde4fe501712d40af5984d1b376e3b
Joe Gallotta <joeygallotta@gmail.com>
2018-11-04 11:23:46 -0600

    add html show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
     @products = Product.all
     render 'index.html.erb'
   end
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
 end
+
+
+# @product = Product.new(name: "remote control", price: 10, description: "universal remote")
+#       @product.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/products' => 'products#index'
+  post '/products/new' => 'products#new'
+  get 'products/:id' => 'products#show'
+
 
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
