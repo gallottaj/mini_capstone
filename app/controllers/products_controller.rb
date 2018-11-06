8ae092651a9d6b2f67fd0e1fc1edc093d483e4bd
Joe Gallotta <joeygallotta@gmail.com>
2018-11-05 19:12:47 -0600

    create edit,update and destroy actions in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,14 +12,31 @@ class ProductsController < ApplicationController
   end
   def create
     @product = Product.new(
-      name: params[:name],
-      price: params[:price],
+      name: params[:name], price: params[:price],
       description: params[:description], 
       supplier_id: 1,
     )
     @product.save!
     redirect_to "/products"
   end
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    # @product.supplier_id = params[:supplier_id] || @product.supplier_id
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
-
+ 
 
